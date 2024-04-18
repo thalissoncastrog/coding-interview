d0b7586c6caca1107006b19393332463cf230c7f
Thalisson Castro <adaothalisson@hotmail.com>
2024-04-18 01:47:11 -0300

    quicksort don't work yet

--- sorting/quicksort/quicksort.py
+++ sorting/quicksort/quicksort.py
@@ -1,9 +1,9 @@
 def quicksort(arr):
-    pivot = arr[len(arr) - 1]    
+    
+    
+    pivot = arr[len(arr) - 1]
+
     arr = partition(arr, pivot, low = 0, high = 0)
-    print("Hello World")
-    # arrLeft = arr1[0:nlow + 1]
-    # arrRight = arr1[nlow + 1:nhigh + 1]
     #arr, low, high = partition(arr, pivot, low = 0, high = 0)
 
     return arr
@@ -16,28 +16,34 @@ def quicksort(arr):
 
 def partition(arr, pivot, low, high):
     
-    for i in range(0, len(arr) - 2):
-        
+    for i in range(0, len(arr) - 1):
+
         if (arr[i] < pivot):
-            
+
             if(i < high):
                 low = i
             else:
                 aux = arr[low] #aux = 3
                 arr[low] = arr[i] # arr[0] = arr[3] = 1
                 arr[i] = aux # arr[3] = 3
-                #low = low + 1 #low = 0 + 1 = 1
-
-
+                low += 1
         high = i
     
-    if(low != high):
-        aux = pivot # 2
-        arr[len(arr) - 1] = arr[low] # arr[4] = arr[0] = 3
-        arr[low] = pivot #arr[0] = 2
-
-    return arr
+    if(low == high):
+        return arr
+    
+    aux = pivot # 2
+    arr[len(arr) - 1] = arr[low] # arr[4] = arr[0] = 3
+    arr[low] = pivot #arr[0] = 2
+    
+    arrLeft = arr[0:low]
+    arrRight = arr[low + 1:len(arr)]
+    arrPivot = [pivot]
+    
+    return quicksort(arrLeft) + arrPivot + quicksort(arrRight)
 
 arr = [3, 7, 5, 1, 2]
 
 result = quicksort(arr)
+
+print(arr)
