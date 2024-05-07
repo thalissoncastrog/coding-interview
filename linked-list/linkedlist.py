15dc6071d817c4313a44f4fbe39887d68d9a1eb4
Thalisson Castro <adaothalisson@hotmail.com>
2024-05-07 17:53:41 -0300

    Created methods to add values in the first node and at a specifc index

--- linked-list/linkedlist.py
+++ linked-list/linkedlist.py
@@ -9,3 +9,35 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+
+    def AddFirst(self, data):
+        newNode = Node(data)
+
+        if self.head is None:
+            self.head = newNode
+            return
+        else:
+            newNode.next = self.head # type: ignore
+            self.head = newNode
+    
+    def AddAtIndex(self, data, index):
+        newNode = Node(data)
+        currentNode = self.head
+        position = 0
+
+        if(position == index):
+            self.AddFirst(data)
+        else:
+            while(currentNode != None and position + 1 != index):
+                position += 1
+                currentNode = currentNode.next
+
+                if currentNode != None:
+                    newNode.next = currentNode.next
+                    currentNode.next = newNode
+                else: 
+                    print("Index not present")
+
+
+
+
